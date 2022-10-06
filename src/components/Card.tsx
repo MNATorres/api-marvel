2d88fb2222695b136cf3aab6b11c9bc80bc75a67
MNATorres <mtri21019@gmail.com>
2022-10-06 00:26:26 -0300

    component card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,14 +5,17 @@ type ElementProps = {
     title: string;
     description: string;
     name: string;
+    extencion: string
 }
 
-const Card: React.FC <ElementProps> = ({image, title, description, name}) => {
+const Card: React.FC <ElementProps> = ({image, title, description, name, extencion}) => {
     return(
         <div className="containerCard">
-            <img src={image} alt={name} />
+            <img src={`${image}.${extencion}`} alt={name} />
+            <div className="containerDesciption">
             <h3>{title}</h3>
             <p>{description}</p>
+            </div>
         </div>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,32 +7,46 @@ import './../styles/HomePage.css';
 //private-key: d501a4af1d9bef85a23f4ed378b7002921c2a8b8
 //ts: 1
 // 1d501a4af1d9bef85a23f4ed378b7002921c2a8b85c596f63542e81287e00e40042a25215
-
 // 1d501a4af1d9bef85a23f4ed378b7002921c2a8b85c596f63542e81287e00e40042a25215
 // http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
 
+interface DataCard {
+    thumbnail: {
+        extension: string; path: string
+    };
+    name: string
+    description: string,
+    id: number,
+
+}
+
 function HomePage() {
-    const [url, setUrl] =useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5c596f63542e81287e00e40042a25215&hash=8771afa70cde304a0c79b966e9b2ffa8");
-    const [item, setItem] = useState();
+    const [url, setUrl] = useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5c596f63542e81287e00e40042a25215&hash=8771afa70cde304a0c79b966e9b2ffa8");
+    const [item, setItem] = useState<DataCard[] | null>();
 
     useEffect(() => {
-        const fetch=async()=>{
-            const res=await axios.get(url)
-            //console.log(res.data.data.results);
+        const fetch = async () => {
+            const res = await axios.get(url)
+            console.log(res.data.data.results);
             setItem(res.data.data.results)
             console.log(item)
         }
         fetch();
-    },[url])
+    }, [url])
 
     return (
         <div className="containerHome">
-            <Card 
-            image= "string"
-            title= "string"
-            description= "string"
-            name= "string"
-            />
+
+            {item?.map((element) => {
+                return <Card
+                    image={element.thumbnail.path}
+                    extencion={element.thumbnail.extension}
+                    title={element.name}
+                    description={element.description}
+                    name={element.name}
+                />
+            })
+            }
         </div>
     );
 }
