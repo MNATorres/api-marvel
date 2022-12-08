14e1a06f7445d202640a2ed403a781b40b028d9e
MNATorres <mtri21019@gmail.com>
2022-12-08 12:45:02 -0300

    favicon personalizados

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -4,7 +4,7 @@ import Video from './Video';
 
 export default function ParralaxContainer() {
     return (
-        <Parallax blur={1} bgImage="https://www.wallpapertip.com/wmimgs/66-662113_desktop-dark-backgrounds-4k.png" bgImageAlt="the cat" strength={200} style={{margin: '100px 0 200px'}}>
+        <Parallax blur={1} bgImage="https://www.wallpapertip.com/wmimgs/66-662113_desktop-dark-backgrounds-4k.png" bgImageAlt="the cat" strength={400} style={{margin: '100px 0 200px'}}>
             <div className="containerParallax" style={{padding: '50px 0'}}>
                 <TitleAnimation />
                 <Video />
